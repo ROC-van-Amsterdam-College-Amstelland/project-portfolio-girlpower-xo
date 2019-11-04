e9cbd145b84fde8262195e2373d892ad42e472ed
Iris Petersen <irispe2707@gmail.com>
2019-11-04 13:26:23 +0100

    dag 1

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function verander1() {
-    document.getElementById("imgIris").style.display ="none";
-    document.getElementById("imgIris2").style.display="block";
+function verander1(plaatje) {
+
+    if(plaatje == 1){
+        document.getElementById("imgIris").style.display ="none";
+        document.getElementById("imgIris2").style.display="block";
+
+    }else{
+        document.getElementById("imgIris").style.display ="block";
+        document.getElementById("imgIris2").style.display="none";
+    }
 }
